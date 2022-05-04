fff538626f65b7987a6ea430562d4db8cd7bd747
Thowhid31 <thowhidantor16@gmail.com>
2022-05-04 12:14:13 +0600

    set all single data to load when a update button cliked

--- src/Pages/SharedPages/ProductArticle/ProductArticle.js
+++ src/Pages/SharedPages/ProductArticle/ProductArticle.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 import './ProductArticl.css'
 
@@ -7,9 +7,27 @@ const ProductArticle = () => {
     
 
     const {productId} = useParams();
+    const [product, setProduct] = useState({});
+
+    useEffect(()=>{
+        const url = `http://localhost:5000/product/${productId}`;
+        fetch(url)
+        .then(res => res.json())
+        .then(data => setProduct(data));
+    }, [])
+
     return (
         <div>
-            <h3>Detail Inventory Article: {productId}</h3>
+            <h3>Detail Inventory Article</h3>
+            <div className='container single-product-details '>
+            <h4 className='m-3'>Name: {product.name}</h4>
+            <img src={product.img} alt="" />
+            <p className='mt-3'><strong>About Product:</strong> {product.description}</p>
+            <p><strong>Price:</strong> {product.price}</p>
+            <p> <strong>Quantity:</strong> {product.quantity}</p>
+            <h5>Supplier: {product.supplierName}</h5>
+            <button className='btn btn-success mb-3 mt-3'>Deliver</button>
+            </div>
             
         </div>
     );
